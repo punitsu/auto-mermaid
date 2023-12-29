174664b42fe33dae15d47a445e9eb04a083d9ec9
Punit <punit.sureka@talview.com>
2023-12-29 19:21:32 +0530

    lint fix

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,17 @@
-const hq = require('alias-hq');
-
 module.exports = {
   roots: ['<rootDir>/engine/'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  moduleNameMapper: hq.get('jest'),
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/engine/src/config/$1',
+    '^@engine/(.*)$': '<rootDir>/engine/src/$1',
+    '^@models/(.*)$': '<rootDir>/engine/src/models/$1',
+    '^@utils/(.*)$': '<rootDir>/engine/src/utils/$1',
+    '^@libs/(.*)$': '<rootDir>/engine/src/libs/$1',
+  },
   coverageDirectory: './coverage',
   watchPathIgnorePatterns: ['<rootDir>/node_modules/'],
 };
